3079b9ca92e2e04f2de60a21408236edd4838231
Daniel Madejek <daniel.madejek@cognifide.com>
2016-07-12 07:57:25 +0200

    Remove unneccesary comments

--- src/main/resources/archetype-resources/src/main/java/GuiceModule.java
+++ src/main/resources/archetype-resources/src/main/java/GuiceModule.java
@@ -10,12 +10,11 @@ import com.google.inject.AbstractModule;
 /**
  * Your Guice module.
  * <p/>
- * The three modules that are installed here are probably the minimum that you'll want to have in
- * your project.
+ * The two modules that are installed here are probably the minimum that you'll want to have in your
+ * project.
  * <p/>
- * CoreModule -- all core functionality, like scope PageObjects or FrameSwitcher. AemClassicModule
- * -- all common AEM elements. ReporterModule -- reporting functionality, including reporting rule
- * and HTML report.
+ * CoreModule -- all core functionality, like scope PageObjects or FrameSwitcher. ReporterModule --
+ * reporting functionality, including reporting rule and HTML report.
  */
 public class GuiceModule extends AbstractModule {
   @Override
